96e5eb2b56d65dc61bd15928c40619b8ba8b2628
ali saleh <oy21115@bristol.ac.uk>
2023-02-20 10:44:30 +0000

    merged

--- week172.py
+++ week172.py
@@ -1,9 +1,4 @@
 print('Hi there')
-<<<<<<< HEAD
 
+print('This is my change my ')
 
-
-print('This is my change')
-=======
-print('change third line.')
->>>>>>> ae14ceb08fa575e1fdfdd36743f7827e5158944d
